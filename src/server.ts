24a95d116ba2def76fe56b1a825e2647d4c1d0a5
mosiur73 <mosiurrahman.cmt173@gmail.com>
2025-12-18 12:32:50 +0600

    add cors origin

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,16 @@ const PORT = process.env.PORT || 5000;
 connectDB()
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://project-pilot-puce.vercel.app",
+     
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
